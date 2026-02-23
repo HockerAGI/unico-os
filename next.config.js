1562d54b653577f89ef7c6037da55d13d6c8690e
Hocker AGI <contacto.hocker@gmail.com>
2026-02-23 05:48:45 -0800

    Actualizar next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [{ protocol: "https", hostname: "lpbzndnavkbpxwnlbqgb.supabase.co" }]
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co', // Permite cargar fotos del Storage de Supabase
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
